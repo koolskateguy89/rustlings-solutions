0fe1bb0f553ec2b486fb2d3f01ca4d2c90610ac7
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-05-24 18:37:51 +0100

    initial commit

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n_apples: i32) -> i32 {
+    if n_apples > 40 {
+        n_apples
+    } else {
+        n_apples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
